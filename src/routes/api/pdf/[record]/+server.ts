963ba4654bf4e0dbf6f30bbd73306a8da6284705
Christine <christinejoyestrella1124@gmail.com>
2024-07-11 15:10:23 +0800

    asd

--- src/routes/api/pdf/[record]/+server.ts
+++ src/routes/api/pdf/[record]/+server.ts
@@ -1,4 +1,5 @@
 import { generatePDF } from '$lib/server/pdf';
+import type { EmployeeInfo, RecordAppointment, Service, ServiceRecord } from '$lib/types';
 import type { TDocumentInformation } from 'pdfmake/interfaces';
 
 export const GET = async ({ params }) => {
@@ -6,9 +7,35 @@ export const GET = async ({ params }) => {
 		author: 'John Doe',
 		title: params.record.toString()
 	};
-	const pdf = await generatePDF(params.record.toString(), metadata);
+//post request = sumbit response
+	const doc: ServiceRecord = {
+		stationnum: "123",
+		empnum: "1111",
+		gsis: "ddf4"
+	}
+
+	const info: EmployeeInfo ={
+		firstName: 'sgsgsf',
+		lastName: 'gsgffg',
+		middleName: 'gsgs',
+		birthdate: new Date('2002-11-02'),
+		placeOfBirth: 'tbc'
+	}	
+	const serv: Service ={
+	
+	}
+	
+	const appnt: RecordAppointment = {
+		position: "bhjfa",
+		status: "jafjf",
+		salary: 122222
+	}
+	
+//should not be duplicate
+	const pdf = await generatePDF(doc,info, metadata);
 
 	return new Response(pdf, {
+		status: 200,
 		headers: {
 			'Content-Type': 'application/pdf',
 			'Content-Length': pdf.size.toString(),
